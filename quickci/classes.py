8244460c6094c37ec3c693cc196404197674be9e
Roberto Preste <roberto.preste@gmail.com>
2019-04-20 14:58:38 +0200

    Update code and add stubs for Codeship and ReadTheDocs

---
+++ quickci/classes.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Created by Roberto Preste
+import json
+import os
+import requests
+from typing import List, Tuple
+
+
+class TravisCI:
+    """
+    Class used to get and manipulate data from the TravisCI platform.
+    """
+
+    def __init__(self, token: str):
+        """
+
+        :param str token: authentication token provided by Travis CI
+        """
+        self.token = token
+        self._url = "https://api.travis-ci.com"
+
+    @property
+    def headers(self):
+        return {"Travis-API-Version": "3",
+                "User-Agent": "CI-Board",
+                "Authorization": "token {}".format(self.token)}
+
+    def user_info(self):
+        q = requests.get(self._url + "/user", headers=self.headers)
+        return q.json()
+
+    def builds(self):
+        q = requests.get(self._url + "/builds", headers=self.headers)
+        return q.json()
+
+    def repos_ids(self):
+        login = self.user_info().get("login")
+        q = requests.get(self._url + "/owner/{}/repos?repository.active=True".format(login),
+                         headers=self.headers)
+        return [(el["name"], el["id"]) for el in q.json().get("repositories")]
+
+    def status(self):
+        res = []
+        for el in self.repos_ids():
+            q = requests.get(self._url
+                             + "/repo/{}/builds?branch.name=master&sort_by=id:desc".format(el[1]),
+                             headers=self.headers)
+            state = q.json().get("builds")[0].get("state")
+            res.append((el[0], state))
+        return res
+
+
+class CircleCI:
+    """
+    Class used to get and manipulate data from the CircleCI platform.
+    """
+
+    def __init__(self, token: str):
+        """
+
+        :param str token: authentication token provided by CircleCI
+        """
+        self.token = token
+        self._url = "https://circleci.com/api/v1.1"
+
+    @property
+    def headers(self):
+        return {"circle-token": self.token}
+
+    def user_info(self):
+        q = requests.get(self._url + "/me?", headers=self.headers)
+        return q.json()
+
+    def projects(self):
+        q = requests.get(self._url + "/projects?", headers=self.headers)
+        return q.json()
+
+    def status(self) -> List[Tuple[str, str]]:
+        """
+        Return the status of each project present on CircleCI (master
+        branch only).
+        :return: List[Tuple[str, str]]
+        """
+        resp = self.projects()
+        return [(repo["reponame"],
+                repo["branches"]["master"]["latest_workflows"]["workflow"]["status"])
+                for repo in resp]
+
+
+class AppVeyor:
+    """
+    TODO: Class used to get and manipulate data from the AppVeyor platform.
+    """
+
+    def __init__(self, token: str):
+        """
+
+        :param str token: authentication token provided by AppVeyor
+        """
+        self.token = token
+        self._url = "https://ci.appveyor.com/api"
+
+    @property
+    def headers(self):
+        return {"Authentication": "Bearer {}".format(self.token)}
+
+
+class Codeship:
+    """
+    TODO: Class used to get and manipulate data from the Codeship platform.
+    """
+
+    def __init__(self, token: str):
+        """
+
+        :param str token: authentication token provided by Codeship
+        """
+        self.token = token
+        self._url = "https://api.codeship.com/v2"
+
+    @property
+    def headers(self):
+        return {"Authentication": self.token}
+
+
+class ReadTheDocs:
+    """
+    TODO: Class used to get and manipulate data from the ReadTheDocs platform.
+    """
+
+    def __init__(self, token: str):
+        """
+
+        :param str token: authentication token provided by ReadTheDocs
+        """
+        self.token = token
+        self._url = "https://readthedocs.org/api/v2"
+
+    @property
+    def headers(self):
+        return {"Authentication": self.token}
+
+
+class Config:
+    """
+    Class that controls the config file used to store and retrieve tokens.
+    """
+
+    def __init__(self):
+        self.config_dir = os.path.expanduser("~/.config/quickci")
+        self.config_file = "tokens.json"
+        self.content = """{
+    "TRAVISCI_TOKEN": "replace_me", 
+    "CIRCLECI_TOKEN": "replace_me", 
+    "APPVEYOR_TOKEN": "replace_me", 
+    "CODESHIP_TOKEN": "replace_me", 
+    "RTD_TOKEN": "replace_me"
+}"""
+
+    @property
+    def config_path(self):
+        return os.path.join(self.config_dir, self.config_file)
+
+    @staticmethod
+    def check_dir(config_dir: str) -> bool:
+        """
+        Check whether the config dir exists or not.
+        :param str config_dir: config dir to check
+        :return: bool
+        """
+        return os.path.isdir(config_dir)
+
+    @staticmethod
+    def check_file(config_file: str) -> bool:
+        """
+        Check whether the config file exists or not.
+        :param str config_file: config file to check
+        :return: bool
+        """
+        return os.path.isfile(config_file)
+
+    def create_config(self):
+        """
+        Create the config file in the default config dir.
+        :return:
+        """
+        # TODO: check existence of config dir and file first
+        if not self.check_dir(self.config_dir):
+            os.makedirs(self.config_dir)
+        with open(self.config_path, "w") as f:
+            f.write(self.content)
+
+        return
+
+    def parse_config(self):
+        """
+        Parse the config file, if present.
+        :return:
+        """
+        if self.check_file(self.config_path):
+            with open(self.config_path) as f:
+                return json.loads(f.read())
+
+
+
+
--- quickci/cli.py
+++ quickci/cli.py
@@ -3,14 +3,77 @@
 # Created by Roberto Preste
 import sys
 import click
+from quickci.classes import Config, TravisCI, CircleCI, AppVeyor, Codeship, \
+    ReadTheDocs
 
 
-@click.command()
-def main(args=None):
-    """Console script for quickci."""
-    click.echo("Replace this message by putting your code into "
-               "quickci.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+@click.group()
+@click.version_option()
+def main():
+    pass
+
+
+@main.command()
+def config():
+    """
+    Create the config file.
+    """
+    c = Config()
+    click.echo("Creating config file in {}...".format(c.config_path))
+    c.create_config()
+    click.echo("Done. Please replace temp tokens as needed.")
+
+    return 0
+
+
+@main.command()
+@click.option("--travis", "-t", help="""TravisCI auth token""")
+@click.option("--circle", "-c", help="""CircleCI auth token""")
+@click.option("--appveyor", "-a", help="""AppVeyor auth token""")
+@click.option("--codeship", "-s", help="""Codeship auth token""")
+@click.option("--readthedocs", "-r", help="""ReadTheDocs auth token""")
+def status(travis, circle, appveyor, codeship, readthedocs):
+    """
+    Return the status of the master branch of each project in each CI.
+    """
+    conf_tokens = Config().parse_config()
+    if travis:
+        t = TravisCI(token=travis)
+    else:
+        t = TravisCI(token=conf_tokens["TRAVISCI_TOKEN"])
+    if circle:
+        c = CircleCI(token=circle)
+    else:
+        c = CircleCI(token=conf_tokens["CIRCLECI_TOKEN"])
+    if appveyor:
+        a = AppVeyor(token=appveyor)
+    else:
+        a = AppVeyor(token=conf_tokens["APPVEYOR_TOKEN"])
+    if codeship:
+        s = Codeship(token=codeship)
+    else:
+        s = Codeship(token=conf_tokens["CODESHIP_TOKEN"])
+    if readthedocs:
+        r = ReadTheDocs(token=readthedocs)
+    else:
+        r = ReadTheDocs(token=conf_tokens["RTD_TOKEN"])
+    # click.echo(t.get_user_info())
+    # click.echo(t.get_builds())
+    # click.echo(c.get_user_info())
+    # click.echo(c.projects())
+    fg_cols_circle = {"success": "green",
+                      "running": "yellow",
+                      "failed": "red"}
+    fg_cols_travis = {"passed": "green",
+                      "failed": "red",
+                      "errored": "red"}
+    click.secho("CircleCI", bold=True, fg="blue")
+    for el in c.status():
+        click.secho("\t{} -> {}".format(el[0], el[1]), fg=fg_cols_circle[el[1]])
+    click.secho("Travis CI", bold=True, fg="blue")
+    for el in t.status():
+        click.secho("\t{} -> {}".format(el[0], el[1]), fg=fg_cols_travis[el[1]])
+
     return 0
 
 
