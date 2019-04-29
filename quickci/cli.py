94285c43fe183866981bc0ca8976f1a131d69439
Roberto Preste <roberto.preste@gmail.com>
2019-04-29 20:24:38 +0200

    Update CLI functions

--- quickci/cli.py
+++ quickci/cli.py
@@ -14,14 +14,34 @@ def main():
 
 
 @main.command()
-def config():
+@click.option("--show", "-s", is_flag=True, default=False,
+              help="""Show current tokens""")
+@click.option("--update", "-u", nargs=2, type=str,
+              help="""Update existing tokens""")
+@click.option("--create", "-c", is_flag=True, default=False,
+              help="""Create empty config file""")
+def config(show, update, create):
     """
-    Create the config file.
+    Create an empty configuration file, or retrieve and update tokens
+    from an existing one.
     """
-    c = Config()
-    click.echo("Creating config file in {}...".format(c.config_path))
-    c.create()
-    click.echo("Done. Please replace temp tokens as needed.")
+    conf = Config()
+    if show:
+        click.echo(conf.show())
+    if create:
+        if conf.check_file():
+            click.echo("Configuration file already present in {}".format(conf.config_path))
+            if not click.confirm("Do you really want to overwrite it?",
+                                 prompt_suffix=" "):
+                return 0
+        click.echo("Creating empty config file in {}... ".format(conf.config_path), nl=False)
+        conf.create()
+        click.echo("Done.")
+        click.echo("Please replace temporary tokens as needed.")
+    if update:
+        conf.update(update[0], update[1])
+        conf.save()
+        click.echo("Updated token for {}.".format(update[0]))
 
     return 0
 
@@ -36,31 +56,28 @@ def status(travis, circle, appveyor, codeship, readthedocs):
     """
     Return the status of the master branch of each project in each CI.
     """
-    conf_tokens = Config().parse()
+    conf = Config().parse()
     if travis:
         t = TravisCI(token=travis)
     else:
-        t = TravisCI(token=conf_tokens["TRAVISCI_TOKEN"])
+        t = TravisCI(token=conf["TRAVISCI_TOKEN"])
     if circle:
         c = CircleCI(token=circle)
     else:
-        c = CircleCI(token=conf_tokens["CIRCLECI_TOKEN"])
+        c = CircleCI(token=conf["CIRCLECI_TOKEN"])
     if appveyor:
         a = AppVeyor(token=appveyor)
     else:
-        a = AppVeyor(token=conf_tokens["APPVEYOR_TOKEN"])
+        a = AppVeyor(token=conf["APPVEYOR_TOKEN"])
     if codeship:
         s = Codeship(token=codeship)
     else:
-        s = Codeship(token=conf_tokens["CODESHIP_TOKEN"])
+        s = Codeship(token=conf["CODESHIP_TOKEN"])
     if readthedocs:
         r = ReadTheDocs(token=readthedocs)
     else:
-        r = ReadTheDocs(token=conf_tokens["RTD_TOKEN"])
-    # click.echo(t.get_user_info())
-    # click.echo(t.get_builds())
-    # click.echo(c.get_user_info())
-    # click.echo(c.projects())
+        r = ReadTheDocs(token=conf["RTD_TOKEN"])
+
     click.secho("CircleCI", bold=True, fg="blue")
     for el in c.status():
         click.secho("\t{} -> {}".format(el[0], el[1]), fg=c.colours[el[1]])
